15d2b4b02e9e5356469ca6cb72048c5cdb4376c9
Gonzalo Encinas <dulphyj@gmail.com>
2025-05-11 21:42:55 -0400

    added initial backend, added UML diagrams

---
+++ Backend/src/main/java/com/dlph/ArchGen/ArchGenApplication.java
@@ -0,0 +1,13 @@
+package com.dlph.ArchGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchGenApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/dlph/ArchGen/ArchGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlph.ArchGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
