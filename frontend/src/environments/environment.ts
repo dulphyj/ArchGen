9aaa517afcdaf5f121750216f88cc0d8d45fd7dd
Gonzalo Encinas <dulphyj@gmail.com>
2025-05-20 17:39:05 -0400

    fixed some diagrams

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     backendUrl: 'http://localhost:9000',
     clerkApiKey: 'pk_test_cXVpY2stZWxlcGhhbnQtNTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
