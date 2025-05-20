edc4d745fd906ac799b93bcd5779daff631121a4
Gonzalo Encinas <dulphyj@gmail.com>
2025-05-20 14:04:16 -0400

    added docker

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClerkService } from 'ngx-clerk-iliad';
 import { environment } from '../environments/environment';
+import { shadesOfPurple } from '@clerk/themes';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,10 @@ export class AppComponent {
   title = 'ArchGen';
   constructor(private _clerk: ClerkService) {
     this._clerk.__init({
-      publishableKey: environment.clerkApiKey
+      publishableKey: environment.clerkApiKey,
+      appearance: {
+        baseTheme: shadesOfPurple
+      }
     })
     this._clerk.clerk$.subscribe(clerk => {
       if (clerk) {
