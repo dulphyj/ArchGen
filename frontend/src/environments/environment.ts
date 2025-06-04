8b37ab7988a1721545798678e83d98b722aef362
Gonzalo Encinas <dulphyj@gmail.com>
2025-06-03 21:17:16 -0400

    try dockerfile front

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:9000',
+    backendUrl: 'https://20.94.243.1:9000',
     clerkApiKey: 'pk_test_cXVpY2stZWxlcGhhbnQtNTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
