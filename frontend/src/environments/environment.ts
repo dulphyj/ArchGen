0a08c3ea04e139ef35fcb84cfa75f00794cb409c
Gonzalo Encinas <dulphyj@gmail.com>
2025-06-06 00:01:16 -0400

    change compose

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'https://20.94.243.1:9000',
+    backendUrl: 'https://archgen.site/api',
     clerkApiKey: 'pk_test_cXVpY2stZWxlcGhhbnQtNTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
