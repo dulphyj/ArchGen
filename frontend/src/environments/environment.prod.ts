c3614ec94837e93e169d8e80df34299ae24f69aa
Gonzalo Encinas <dulphyj@gmail.com>
2025-06-07 09:26:08 -0400

    edit clerk

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     backendUrl: '/api',
-    clerkApiKey: 'pk_test_cXVpY2stZWxlcGhhbnQtNTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
+    clerkApiKey: '',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'https://archgen.site/api',
+    backendUrl: 'https://localhost:9000/api',
     clerkApiKey: 'pk_test_cXVpY2stZWxlcGhhbnQtNTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
 };
